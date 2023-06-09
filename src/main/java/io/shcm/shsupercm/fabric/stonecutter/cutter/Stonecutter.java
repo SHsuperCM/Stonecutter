4cc57c181cf949ff501ff0aa0bf9d2626cc75b19
SHsuperCM <shsupercm@gmail.com>
2023-06-09 07:44:02 +0300

    Made set active task switch sourcesets

--- src/main/java/io/shcm/shsupercm/fabric/stonecutter/cutter/Stonecutter.java
+++ src/main/java/io/shcm/shsupercm/fabric/stonecutter/cutter/Stonecutter.java
@@ -4,6 +4,12 @@ import io.shcm.shsupercm.fabric.stonecutter.StonecutterBuildGradle;
 import org.gradle.api.Project;
 import org.gradle.api.Task;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Stonecutter {
     private final Project controllerProject;
     private final StonecutterBuildGradle.VersionData fromVersion;
@@ -18,6 +24,17 @@ public class Stonecutter {
     public void run(Task task) {
         task.getLogger().quiet((toVersion.isActiveVersion() ? "> Reloading active Stonecutter version " : "> Switching active Stonecutter version to ") + "'" + toVersion.version() + "'");
 
-        
+        try {
+            switchSourceSet();
+
+        } catch (Exception exception) {
+            throw new RuntimeException(exception);
+        }
+    }
+
+    public void switchSourceSet() throws IOException {
+        List<String> stonecutterGradleLines = new ArrayList<>(Files.readAllLines(controllerProject.getBuildFile().toPath()));
+        stonecutterGradleLines.set(5, "stonecutter.current('" + toVersion.version() + "')");
+        Files.write(controllerProject.getBuildFile().toPath(), stonecutterGradleLines, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
     }
 }
