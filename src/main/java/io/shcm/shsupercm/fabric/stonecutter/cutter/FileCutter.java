b0d68d58d825d88cc93ff13e56662db8261296db
SHsuperCM <shsupercm@gmail.com>
2023-06-10 13:01:55 +0300

    Implemented syntax logic

--- src/main/java/io/shcm/shsupercm/fabric/stonecutter/cutter/FileCutter.java
+++ src/main/java/io/shcm/shsupercm/fabric/stonecutter/cutter/FileCutter.java
@@ -13,7 +13,65 @@ public class FileCutter {
         this.stonecutter = stonecutter;
     }
 
+    private BufferedReader oldContents;
+    private StringBuilder newContents;
+
+    private boolean versionedCode = false, disabledCode = false;
+
     public void apply() throws Exception {
-        BufferedReader reader = Files.newBufferedReader(file.toPath(), stonecutter.charset());
+        newContents = new StringBuilder();
+        try (BufferedReader oldContents = this.oldContents = Files.newBufferedReader(file.toPath(), stonecutter.charset())) {
+            while (find("/*?")) {
+                String expression = read("?*/");
+                if (expression == null)
+                    throw new StonecutterSyntaxException("Expected ?*/");
+                expression = expression.trim();
+
+                if (expression.startsWith("}")) { // closing versioned code
+                    if (!versionedCode)
+                        throw new StonecutterSyntaxException("Unexpected } closing non-versioned code");
+
+                    versionedCode = false;
+                    expression = expression.substring(1).stripLeading();
+                } else if (expression.startsWith("{"))
+                    throw new StonecutterSyntaxException("Unexpected { opening versioned code without a condition");
+
+                if (!expression.isBlank()) {
+                    boolean els = false;
+                    if (expression.startsWith("else")) { // if not previous condition
+                        expression = expression.substring(4).stripLeading();
+
+                        els = true;
+                    } else if (expression.startsWith("{"))
+                        throw new StonecutterSyntaxException("Unexpected { opening versioned code without a condition");
+
+                    if (expression.endsWith("{")) {
+                        versionedCode = true;
+                        expression = expression.substring(0, expression.length() - 1).stripTrailing();
+                    }
+
+                    if (expression.isBlank()) {
+                        if (els)
+                            disabledCode = !disabledCode;
+                        else
+                            throw new StonecutterSyntaxException("Unexpected { opening versioned code without a condition");
+                    } else
+                        disabledCode = (els && !disabledCode) || !stonecutter.testVersion(expression);
+
+                    //todo append enable/disable code part
+                }
+            }
+        }
+
+        //todo write new contents to file
+    }
+
+    private String read(String match) {
+        //todo implement
+        throw new java.lang.UnsupportedOperationException();
+    }
+
+    private boolean find(String match) {
+        return read(match) != null;
     }
 }
---
+++ src/main/java/io/shcm/shsupercm/fabric/stonecutter/cutter/StonecutterSyntaxException.java
@@ -0,0 +1,7 @@
+package io.shcm.shsupercm.fabric.stonecutter.cutter;
+
+public class StonecutterSyntaxException extends Exception {
+    public StonecutterSyntaxException(String message) {
+        super(message);
+    }
+}
