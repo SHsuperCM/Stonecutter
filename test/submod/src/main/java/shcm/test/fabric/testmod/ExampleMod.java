a3255d066c0e120326cab057a727c877117e2eed
SHsuperCM <shsupercm@gmail.com>
2023-06-10 10:53:40 +0300

    Changed proposed syntax example

--- test/submod/src/main/java/shcm/test/fabric/testmod/ExampleMod.java
+++ test/submod/src/main/java/shcm/test/fabric/testmod/ExampleMod.java
@@ -7,41 +7,40 @@ import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-//? >=1.19.4 {
+/*? >=1.19.4 {?*/
 import net.minecraft.registry.Registries;
-/*?}*//*? else {
+/*?} else {?*//*
 import net.minecraft.util.registry.Registry;
-//?}*/
+/*?}?*/
 
 public class ExampleMod implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
     public static final Logger LOGGER = LoggerFactory.getLogger("testmod");
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("Example of inline versioned value getter");
+		Item item = /*? >=1.19.4 {?*/ Registries.ITEM.get(new Identifier("minecraft:flint")) /*?} else {?*//* Registry.ITEM.get(new Identifier("minecraft:flint")) /*?}?*/;
 
-		Item item;
-
-		//? >=1.19.4 {
+		LOGGER.info("Example of 'only run in this version constraint' syntax");
+		/*? ~1.20 {?*/
 		item = Registries.ITEM.get(new Identifier("minecraft:flint"));
-		/*?}*//*? else {
+		/*?}?*/
+
+		LOGGER.info("Example of 'if else' syntax");
+		/*? >=1.19.4 {?*/
+		item = Registries.ITEM.get(new Identifier("minecraft:flint"));
+		/*?} else {?*//*
 		item = Registry.ITEM.get(new Identifier("minecraft:flint"));
-		//?}*/
+		/*?}?*/
 
-
-		//? >=1.19.4 {
-		item = Registries.ITEM.get(new Identifier("minecraft:flint"));
-		/*?}*//*? ~1.19.3 {
-		item = Registries.ITEM.get(new Identifier("minecraft:flint"));
-		/*?}*//*? else {
-		item = Registry.ITEM.get(new Identifier("minecraft:flint"));
-		//?}*/
+		LOGGER.info("Example of 'if elseif else' syntax and how to avoid versioned imports");
+		/*? >=1.19.4 {?*/
+		item = net.minecraft.registry.Registries.ITEM.get(new Identifier("minecraft:flint"));
+		/*?} else ~1.19.3 {?*//*
+		item = net.minecraft.registry.Registries.ITEM.get(new Identifier("minecraft:flint"));
+		/*?} else {?*//*
+		item = net.minecraft.util.registry.Registry.ITEM.get(new Identifier("minecraft:flint"));
+		/*?}?*/
 	}
 }
